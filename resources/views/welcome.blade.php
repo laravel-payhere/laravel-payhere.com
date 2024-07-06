89c0f58471d1e854dcf8f024726169820c6b08bc
Dasun Tharanga <hello@dasun.dev>
2024-07-06 18:21:06 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,15 @@
 @extends("layouts.app")
 
 @section("content")
-    <section class="flex flex-col items-center gap-10 py-24">
-        <h1 class="text-5xl font-bold text-white">
-            Pay
-            <span class="text-yellow-500">Here</span>
-            for Laravel
-        </h1>
-        <p class="text-lg text-white">Easily and securely integrate PayHere into your Laravel application with our trusted third-party plugin.</p>
-        <img class="w-full scale-90" src="{{ Vite::asset("resources/images/screenshot.png") }}" alt="Filament panel's screenshot" />
+    <section class="mx-auto max-w-7xl py-24">
+        <div class="flex flex-col items-center gap-10">
+            <h1 class="text-5xl font-bold text-white">
+                Pay
+                <span class="text-yellow-500">Here</span>
+                for Laravel
+            </h1>
+            <p class="text-lg text-white">Easily and securely integrate PayHere into your Laravel application with our trusted third-party plugin.</p>
+            <img class="w-full scale-90" src="{{ Vite::asset("resources/images/screenshot.png") }}" alt="Filament panel's screenshot" />
+        </div>
     </section>
 @endsection
