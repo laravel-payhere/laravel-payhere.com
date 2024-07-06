64e0a74c2ca8c401327a9a3faf52673dc257f450
Dasun Tharanga <hello@dasun.dev>
2024-07-06 15:55:16 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,10 @@
         <div class="bg-gradient-to-b from-blue-700 to-black">
             <img src="{{ Vite::asset("resources/images/hero-background.png") }}" class="opacity-50" alt="Hero Background" />
         </div>
-        <div class="absolute top-0 w-full px-32">
+        <div class="absolute top-0 w-full px-5 md:px-10 lg:px-32">
             <nav class="flex items-center justify-between py-5">
                 <img src="{{ Vite::asset("resources/images/logo@1.svg") }}" class="h-[50px]" alt="Laravel PayHere Logo" />
-                <div class="inline-flex gap-8">
+                <div class="hidden gap-8 lg:inline-flex">
                     <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm text-white transition-colors hover:bg-black hover:bg-opacity-15">Features</a>
                     <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm text-white transition-colors hover:bg-black hover:bg-opacity-15">Docs</a>
                     <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm text-white transition-colors hover:bg-black hover:bg-opacity-15">Changelog</a>
