020d9be5101b1f50522370f970e3be62385bddef
Dasun Tharanga <hello@dasun.dev>
2024-07-06 18:30:20 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     <section class="mx-auto max-w-7xl py-24">
         <div class="flex flex-col items-center gap-10">
             <h1 class="text-5xl font-bold text-white">
-                <div class="inline-flex">
+                <span class="inline-flex">
                     Pay
                     <span class="text-yellow-500">Here</span>
-                </div>
+                </span>
                 for Laravel
             </h1>
             <p class="text-lg text-white">Easily and securely integrate PayHere into your Laravel application with our trusted third-party plugin.</p>
