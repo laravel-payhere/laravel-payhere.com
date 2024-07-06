2fa48cafc953b51e01d66194c6491861063508db
Dasun Tharanga <hello@dasun.dev>
2024-07-06 17:55:13 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.bunny.net" />
-        <link href="https://fonts.bunny.net/css?family=ubuntu:400,500" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=ubuntu:400,500,700" rel="stylesheet" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600" rel="stylesheet" />
 
         <!-- Scripts -->
@@ -22,30 +22,32 @@
             <img src="{{ Vite::asset("resources/images/hero-background.png") }}" class="opacity-50" alt="Hero Background" />
         </div>
         <div class="absolute top-0 w-full px-5 md:px-10 lg:px-32">
-            <nav class="flex items-center justify-between py-5">
-                <img src="{{ Vite::asset("resources/images/logo@1.svg") }}" class="h-[50px]" alt="Laravel PayHere Logo" />
-                <div class="hidden gap-8 lg:inline-flex">
-                    <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black hover:bg-opacity-15">Features</a>
-                    <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black hover:bg-opacity-15">Docs</a>
-                    <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black hover:bg-opacity-15">Changelog</a>
-                    <a href="#" class="cursor-pointer rounded-xl bg-white bg-opacity-10 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-opacity-15">Purchase a license</a>
+            <header>
+                <nav class="flex items-center justify-between py-5">
+                    <img src="{{ Vite::asset("resources/images/logo@1.svg") }}" class="h-[50px]" alt="Laravel PayHere Logo" />
+                    <div class="hidden gap-8 lg:inline-flex">
+                        <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black hover:bg-opacity-15">Features</a>
+                        <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black hover:bg-opacity-15">Docs</a>
+                        <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black hover:bg-opacity-15">Changelog</a>
+                        <a href="#" class="cursor-pointer rounded-xl bg-white bg-opacity-10 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-opacity-15">Purchase a license</a>
+                    </div>
+                </nav>
+            </header>
+
+            @yield("content")
+
+            <footer class="flex flex-col items-center justify-center gap-3 px-5 py-16 md:px-10 lg:px-32">
+                <img src="{{ Vite::asset("resources/images/logo@2.svg") }}" class="h-[30px]" alt="Laravel PayHere Logo" />
+                <div class="inline-flex gap-3">
+                    <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Privacy Policy</a>
+                    <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Terms and Conditions</a>
+                    <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Refund Policy</a>
                 </div>
-            </nav>
+                <div class="text-center text-sm text-white">
+                    <a href="https://payhere.lk/" class="cursor-pointer text-xs font-medium text-white transition-colors hover:underline md:text-sm">PayHere</a>
+                    is a trademark and usage of their trademark for this project has been approved by them.
+                </div>
+            </footer>
         </div>
-
-        @yield("content")
-
-        <footer class="flex flex-col items-center justify-center gap-3 px-5 py-16 md:px-10 lg:px-32">
-            <img src="{{ Vite::asset("resources/images/logo@2.svg") }}" class="h-[30px]" alt="Laravel PayHere Logo" />
-            <div class="inline-flex gap-3">
-                <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Privacy Policy</a>
-                <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Terms and Conditions</a>
-                <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Refund Policy</a>
-            </div>
-            <div class="text-center text-sm text-white">
-                <a href="https://payhere.lk/" class="cursor-pointer text-xs font-medium text-white transition-colors hover:underline md:text-sm">PayHere</a>
-                is a trademark and usage of their trademark for this project has been approved by them.
-            </div>
-        </footer>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
 @extends("layouts.app")
+
+@section("content")
+    <section class="py-16">
+        <h1 class="text-center font-['Ubuntu'] text-5xl font-bold text-white">
+            Pay
+            <span class="text-yellow-500">Here</span>
+            for Laravel
+        </h1>
+    </section>
+@endsection
