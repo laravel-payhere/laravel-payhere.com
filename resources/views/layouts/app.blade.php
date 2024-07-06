0a4fa31bdf1dc810b3dbe43e427d4db336e468a2
Dasun Tharanga <hello@dasun.dev>
2024-07-06 15:43:45 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <div class="absolute top-0 w-full px-32">
             <nav class="flex items-center justify-between py-5">
                 <img src="{{ Vite::asset("resources/images/logo@1.svg") }}" alt="Laravel PayHere Logo" />
-                <div class="inline-flex gap-5">
+                <div class="inline-flex gap-8">
                     <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm text-white transition-colors hover:bg-black hover:bg-opacity-15">Features</a>
                     <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm text-white transition-colors hover:bg-black hover:bg-opacity-15">Docs</a>
                     <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-sm text-white transition-colors hover:bg-black hover:bg-opacity-15">Changelog</a>
