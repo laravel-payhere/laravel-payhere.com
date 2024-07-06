f6e6fe9717d93ce51a55968bc3cc06e6e22e96b0
Dasun Tharanga <hello@dasun.dev>
2024-07-06 18:06:41 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 @extends("layouts.app")
 
 @section("content")
-    <section class="py-16">
-        <h1 class="text-center text-5xl font-bold text-white">
+    <section class="flex flex-col items-center gap-10 py-16">
+        <h1 class="text-5xl font-bold text-white">
             Pay
             <span class="text-yellow-500">Here</span>
             for Laravel
         </h1>
+        <p class="text-lg text-white">Easily and securely integrate PayHere into your Laravel application with our trusted third-party plugin.</p>
     </section>
 @endsection
