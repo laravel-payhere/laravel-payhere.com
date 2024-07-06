f10316e6fb4016c77e4b109ef6177aea5fa5fdbd
Dasun Tharanga <hello@dasun.dev>
2024-07-06 18:34:50 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,4 +14,13 @@
             <img class="w-full scale-90" src="{{ Vite::asset("resources/images/screenshot.png") }}" alt="Filament panel's screenshot" />
         </div>
     </section>
+    <section class="mx-auto max-w-7xl py-24">
+        <div class="flex flex-col items-center gap-2">
+            <h2 class="text-3xl text-white">
+                Built with the
+                <strong>TALL Stack</strong>
+            </h2>
+            <p class="text-md text-gray-500">We use the latest technologies to provide you with the best user experience.</p>
+        </div>
+    </section>
 @endsection
