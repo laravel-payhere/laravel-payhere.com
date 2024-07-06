a162910736d12ea3f9671b799052e0ee6152380e
Dasun Tharanga <hello@dasun.dev>
2024-07-06 15:39:37 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,8 @@
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.bunny.net" />
-        <link
-            href="https://fonts.bunny.net/css?family=Ubuntu"
-            rel="stylesheet"
-        />
-        <link
-            href="https://fonts.bunny.net/css?family=Figtree"
-            rel="stylesheet"
-        />
+        <link href="https://fonts.bunny.net/css?family=Ubuntu" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=Figtree" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(["resources/css/app.css", "resources/js/app.js"])
@@ -33,11 +27,13 @@
         </div>
         <div class="absolute top-0 w-full px-32">
             <nav class="flex items-center justify-between py-5">
-                <img
-                    src="{{ Vite::asset("resources/images/logo@1.svg") }}"
-                    alt="Laravel PayHere Logo"
-                />
-                <div class="inline-flex gap-10">
+                <img src="{{ Vite::asset("resources/images/logo@1.svg") }}" alt="Laravel PayHere Logo" />
+                <div class="inline-flex gap-5">
+                    <a
+                        class="cursor-pointer rounded-full px-5 py-2 text-white transition-colors hover:bg-black hover:bg-opacity-15"
+                    >
+                        Features
+                    </a>
                     <a
                         class="cursor-pointer rounded-full px-5 py-2 text-white transition-colors hover:bg-black hover:bg-opacity-15"
                     >
