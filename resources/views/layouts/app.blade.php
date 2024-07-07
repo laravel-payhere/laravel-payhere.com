2471222b8c2480c1020e39c8e33791312043222c
Dasun Tharanga <hello@dasun.dev>
2024-07-07 21:18:06 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,17 +41,17 @@
                     @yield("content")
                 </main>
 
-                <footer class="mt-20 flex flex-col items-center justify-center gap-3 py-16">
+                <footer class="mt-20 flex flex-col items-start gap-3 py-8 md:items-center md:gap-5 md:py-16">
                     <a href="{{ config("app.url") }}">
-                        <img src="{{ Vite::asset("resources/images/logo@2.svg") }}" class="h-[30px]" alt="Laravel PayHere Logo" />
+                        <img src="{{ Vite::asset("resources/images/logo@2.svg") }}" class="h-5 md:h-8" alt="Laravel PayHere Logo" />
                     </a>
-                    <div class="inline-flex gap-3">
-                        <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Privacy Policy</a>
-                        <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Terms and Conditions</a>
-                        <a href="#" class="cursor-pointer rounded-full px-4 py-2 text-xs font-medium text-white transition-colors hover:underline md:text-sm">Refund Policy</a>
+                    <div class="inline-flex gap-10">
+                        <a href="#" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Privacy Policy</a>
+                        <a href="#" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Terms and Conditions</a>
+                        <a href="#" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Refund Policy</a>
                     </div>
-                    <div class="text-center text-sm text-white">
-                        <a href="https://payhere.lk/" class="cursor-pointer text-xs font-medium text-white transition-colors hover:underline md:text-sm">PayHere</a>
+                    <div class="text-start text-sm text-white md:text-center">
+                        <a href="https://payhere.lk/" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">PayHere</a>
                         is a trademark and usage of their trademark for this project has been approved by them.
                     </div>
                 </footer>
