077b6c8f174c7bc5c955874673ff9d9a0f5ea29b
Dasun Tharanga <hello@dasun.dev>
2024-07-07 10:07:24 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.app")
 
 @section("content")
-    <section class="mt-10 lg:mt-20">
+    <section class="my-10 lg:my-20">
         <div class="flex flex-col gap-5 text-start md:items-center lg:gap-10">
             <h1 class="text-3xl font-bold text-white md:text-4xl lg:text-5xl">
                 <span class="inline-flex">
@@ -14,13 +14,13 @@
             <img class="w-full scale-100 md:scale-90" src="{{ Vite::asset("resources/images/screenshot.png") }}" alt="Filament panel's screenshot" />
         </div>
     </section>
-    <section class="mt-20">
+    <section class="py-20">
         <div class="flex flex-col items-center gap-5">
             <h2 class="text-xl text-white md:text-2xl lg:text-3xl">
                 Built with the
                 <strong>TALL Stack</strong>
             </h2>
-            <p class="text-center text-sm text-white opacity-70 md:text-base">We use cutting-edge technologies to deliver the best user experience.</p>
+            <p class="text-center text-xs text-white opacity-70 md:text-sm lg:text-base">We use cutting-edge technologies to deliver the best user experience.</p>
             <div class="mx-auto mt-14 grid w-full max-w-5xl grid-cols-2 items-center justify-between justify-items-stretch gap-y-8 px-8 md:grid-cols-4 md:px-0 xl:flex">
                 <img class="h-7" src="{{ Vite::asset("resources/images/laravel.svg") }}" alt="Laravel" />
                 <img class="h-9" src="{{ Vite::asset("resources/images/alpinejs.svg") }}" alt="Alpine.js" />
