b34db4fbdcf59e683aa1d9565024733acb2d1f37
Dasun Tharanga <hello@dasun.dev>
2024-07-14 19:36:49 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,7 @@
     </section>
     <section class="py-20">
         <h2 class="text-center text-4xl font-bold text-white">Pay once, use forever</h2>
-        <p class="mt-5 text-center text-sm text-white opacity-70 md:text-base">Every purchase includes a perpetual fallback license that you can use forever.</p>
+        <p class="mt-5 text-center text-base text-white opacity-70 md:text-lg">Every purchase includes a perpetual fallback license that you can use forever.</p>
         <div class="mx-auto grid max-w-5xl grid-cols-3">
             <div class="col-span-2 mt-10 flex flex-col bg-gradient-to-l from-blue-700 px-20 py-20">
                 <span class="text-base text-white">For unlimited projects</span>
