6642d6ae27af39b19040333133ae745dd80c87ee
Dasun Tharanga <hello@dasun.dev>
2024-07-27 09:27:00 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,10 +16,7 @@
     </section>
     <section class="px-5 py-20 md:px-0">
         <div class="flex flex-col items-center gap-5">
-            <h2 class="text-xl text-white md:text-2xl lg:text-4xl">
-                Built with the
-                <strong>TALL Stack</strong>
-            </h2>
+            <h2 class="text-xl font-semibold text-white md:text-2xl lg:text-4xl">Built with the TALL Stack</h2>
             <p class="text-center text-sm text-white opacity-70 md:text-lg">We use cutting-edge technologies to deliver the best user experience.</p>
             <div class="mx-auto mt-14 grid w-full max-w-5xl grid-cols-2 items-center justify-between justify-items-stretch gap-y-8 px-8 md:grid-cols-4 md:px-0 xl:flex">
                 <img class="h-7" src="{{ Vite::asset("resources/images/laravel.svg") }}" alt="Laravel" />
