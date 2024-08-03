c4c62dfd0a9aba1d313e9db605801fb6bb23338c
Dasun Tharanga <hello@dasun.dev>
2024-08-03 17:50:09 +0530

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         </div>
     </section>
     <section class="flex flex-col gap-10 px-0 py-20 md:px-10">
-        <div class="bg-feature relative flex gap-5 overflow-hidden rounded-none border border-white border-opacity-20 px-10 py-14 md:rounded-xl xl:px-[80px] xl:py-[80px]">
+        <div class="bg-feature relative flex gap-5 overflow-hidden rounded-none border border-white border-opacity-20 px-5 py-14 md:rounded-xl md:px-10 lg:px-[80px] lg:py-[80px]">
             <div class="max-w-full xl:max-w-2xl">
                 <h3 class="text-xl font-semibold text-white md:text-2xl">Simplified HTML Form Based APIs</h3>
                 <p class="mt-10 text-sm text-white md:text-base">PayHere offers four different types of HTML form-based APIs to suit various needs. One of these is the recurring checkout API for subscriptions (as shown on the right). It's a simple HTML form-based POST API, which you can easily implement without leaving the PHP code base.</p>
@@ -39,7 +39,7 @@
                 <img class="absolute top-5 w-2/5" src="{{ Vite::asset("resources/images/recurring-api-example.png") }}" alt="Recurring API Example" />
             </div>
         </div>
-        <div class="bg-feature-reverse flex items-center justify-between overflow-hidden rounded-none border border-white border-opacity-20 px-10 py-14 md:rounded-xl xl:px-[80px] xl:py-[80px]">
+        <div class="bg-feature-reverse flex items-center justify-between overflow-hidden rounded-none border border-white border-opacity-20 px-5 py-14 md:rounded-xl md:px-10 lg:px-[80px] lg:py-[80px]">
             <img class="hidden h-14 xl:block" src="{{ Vite::asset("resources/images/filament.svg") }}" alt="Filament logo" />
             <div class="max-w-full xl:max-w-2xl">
                 <h3 class="text-xl font-semibold text-white md:text-2xl">Filament Panel for Manage Payments & Subscriptions</h3>
@@ -49,7 +49,7 @@
                 </div>
             </div>
         </div>
-        <div class="bg-feature relative flex gap-10 overflow-hidden rounded-none border border-white border-opacity-20 px-10 py-14 md:rounded-xl xl:px-[80px] xl:py-[80px]">
+        <div class="bg-feature relative flex gap-10 overflow-hidden rounded-none border border-white border-opacity-20 px-5 py-14 md:rounded-xl md:px-10 lg:px-[80px] lg:py-[80px]">
             <div class="max-w-full xl:max-w-2xl">
                 <h3 class="text-xl font-semibold text-white md:text-2xl">Intermediate RESTful API</h3>
                 <p class="mt-10 text-sm text-white md:text-base">PayHere provides RESTful APIs for handling payments, refunds, and subscriptions. Our plugin comes with an intermediate API to easily connect with the PayHere API.</p>
