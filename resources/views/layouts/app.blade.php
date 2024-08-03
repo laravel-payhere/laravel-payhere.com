ace4aed566a991998e91caad9eecd456dfca7f58
Dasun Tharanga <hello@dasun.dev>
2024-08-03 23:39:41 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,12 +33,14 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
                         </button>
-                        <div x-cloak x-show="open" x-transition:enter="transition duration-300 ease-in-out" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" :class="open ? 'fixed top-0 right-0 h-full w-10/12 z-50 bg-black p-10 flex flex-col ml-10' : 'hidden lg:inline-flex'" class="gap-8">
-                            <button type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                                </svg>
-                            </button>
+                        <div x-cloak x-show="open" :class="open ? 'fixed top-0 right-0 h-full w-10/12 z-50 bg-black p-10 flex flex-col ml-10' : 'hidden lg:inline-flex'" class="gap-8" x-transition:enter="transform transition duration-300 ease-in-out" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition duration-300 ease-in-out" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
+                            <div class="inline-flex justify-end">
+                                <button type="button" @click="open = false">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
                             <a href="#" class="rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white hover:bg-opacity-15">Features</a>
                             <a href="#" class="rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white hover:bg-opacity-15">Docs</a>
                             <a href="#" class="rounded-full px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-white hover:bg-opacity-15">Changelog</a>
