f0a989ae91b1dadb239fba10a2b3ced250eb7803
Dasun Tharanga <hello@dasun.dev>
2024-08-04 12:08:06 +0530

    wip

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -17,7 +17,7 @@
         <!-- Scripts -->
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
-    <body class="h-screen bg-gradient-to-b from-blue-700 to-black font-sans antialiased">
+    <body class="h-screen bg-black bg-gradient-to-b from-blue-700 to-black font-sans antialiased">
         <main class="grid h-full grid-cols-3 pl-14">
             <div class="col-span-1 h-full py-10">
                 <a href="{{ config("app.url") }}">
@@ -25,11 +25,11 @@
                 </a>
                 <nav aria-label="breadcrumb" class="mt-10">
                     <ol class="inline-flex items-center gap-2 text-sm">
-                        <li class="font-semibold text-yellow-500"><a href="{{ config("app.url") }}">Home</a></li>
+                        <li class="font-semibold text-white"><a href="{{ config("app.url") }}">Home</a></li>
                         <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.8125 3.125L9.1875 7.5L4.8125 11.875" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
-                        <li class="font-semibold text-white" aria-current="page">Payment</li>
+                        <li class="font-semibold text-yellow-500" aria-current="page">Payment</li>
                     </ol>
                 </nav>
             </div>
