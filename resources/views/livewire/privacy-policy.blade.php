c445f732142c97501b9dd20306e8d4c554d63806
Dasun Tharanga <hello@dasun.dev>
2024-08-10 21:09:35 +0530

    wip

--- resources/views/livewire/privacy-policy.blade.php
+++ resources/views/livewire/privacy-policy.blade.php
@@ -1,4 +1,20 @@
 <div class="mx-auto mt-10 max-w-5xl">
-    <h1 class="mb-6 text-start text-4xl text-white">Privacy Policy</h1>
-    <p class="text-base text-white">At Laravel PayHere, we are committed to safeguarding the privacy and security of our customers' personal information. This Privacy Policy outlines how we collect, use, and protect your data when you visit our website or purchase our plugin. By accessing our website or purchasing our plugin, you agree to the practices described in this policy.</p>
+    <h1 class="my-6 text-start text-4xl text-white">Privacy Policy</h1>
+    <div class="text-base text-white">
+        <p>At Laravel PayHere, we are committed to safeguarding the privacy and security of our customers' personal information. This Privacy Policy outlines how we collect, use, and protect your data when you visit our website or purchase our plugin. By accessing our website or purchasing our plugin, you agree to the practices described in this policy.</p>
+        <h2 class="my-6 text-xl">Information We Collect</h2>
+        <p>When you visit our website, we may collect certain information about you, including:</p>
+        <ul class="my-2 list-disc pl-10">
+            <li>Personal identification information (such as your name and email address) provided voluntarily by you during the checkout process.</li>
+            <li>Payment and billing information necessary to process your orders, including credit card details, which are securely handled by trusted third-party payment processors.</li>
+            <li>Browsing information, such as your IP address, browser type, and device information, collected automatically using cookies and similar technologies.</li>
+        </ul>
+        <h2 class="my-6 text-xl">Use Of Information</h2>
+        <p>We may use the collected information for the following purposes:</p>
+        <ul class="my-2 list-disc pl-10">
+            <li>To process your orders, including issuing licenses.</li>
+            <li>To communicate with you regarding your purchases, provide customer support, and respond to inquiries or requests.</li>
+            <li>To improve our website and plugin based on your feedback.</li>
+        </ul>
+    </div>
 </div>
