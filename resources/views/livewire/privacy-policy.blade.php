0c2eecd69a4c62b387a23d47a6aba87422cd8f1d
Dasun Tharanga <hello@dasun.dev>
2024-08-10 20:42:54 +0530

    wip

--- resources/views/livewire/privacy-policy.blade.php
+++ resources/views/livewire/privacy-policy.blade.php
@@ -1,4 +1,4 @@
-<div class="mx-auto mt-14 max-w-5xl">
+<div class="mx-auto mt-10 max-w-5xl">
     <h1 class="mb-6 text-start text-4xl text-white">Privacy Policy</h1>
     <p class="text-base text-white">At Laravel PayHere, we are committed to safeguarding the privacy and security of our customers' personal information. This Privacy Policy outlines how we collect, use, and protect your data when you visit our website or purchase our plugin. By accessing our website or purchasing our plugin, you agree to the practices described in this policy.</p>
 </div>
