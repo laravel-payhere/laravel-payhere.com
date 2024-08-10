2773df063b7332534d6ee92fe1002d91b10ceac6
Dasun Tharanga <hello@dasun.dev>
2024-08-10 22:01:16 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,7 @@
                     </a>
                     <div class="inline-flex flex-col gap-2 md:flex-row md:gap-10">
                         <a wire:navigate wire:navigate.hover href="{{ route("privacy-policy") }}" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Privacy Policy</a>
-                        <a href="#" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Terms and Conditions</a>
+                        <a wire:navigate wire:navigate.hover href="{{ route("terms") }}" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Terms and Conditions</a>
                         <a href="#" class="text-xs font-medium text-white transition-colors hover:underline md:text-sm">Refund Policy</a>
                     </div>
                     <div class="mt-5 text-start text-xs text-white md:text-center md:text-sm">
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Livewire\PrivacyPolicy;
+use App\Livewire\Terms;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class);
 Route::get('/privacy-policy', PrivacyPolicy::class)->name('privacy-policy');
+Route::get('/terms', Terms::class)->name('terms');
 Route::view('/checkout', 'checkout')->name('checkout');
