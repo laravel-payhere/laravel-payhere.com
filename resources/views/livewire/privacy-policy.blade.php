6c65132d8f02828643f3dfab197a66c1f36c2dc7
Dasun Tharanga <hello@dasun.dev>
2024-08-10 19:59:18 +0530

    wip

---
+++ resources/views/livewire/privacy-policy.blade.php
@@ -0,0 +1,4 @@
+<div class="mx-auto max-w-4xl">
+    <h1 class="mb-12 text-center text-3xl font-semibold text-white">Privacy Policy</h1>
+    <p class="text-lg text-white">I, Dasun Tharanga, operate this website selling Laravel plugins. I am committed to protecting your privacy. This Privacy Policy explains how I collect, use, disclose, and safeguard your information when you visit my website and provide me with your personal data</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Livewire\PrivacyPolicy;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class);
+Route::get('/privacy-policy', PrivacyPolicy::class);
 Route::view('/checkout', 'checkout')->name('checkout');
