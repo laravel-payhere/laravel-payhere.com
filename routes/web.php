79d7d8ac6b5c9ff8cd6fe02852346a8716ddc1ac
Dasun Tharanga <hello@dasun.dev>
2024-08-11 10:35:23 +0530

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Livewire\PrivacyPolicy;
+use App\Livewire\RefundPolicy;
 use App\Livewire\Terms;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class);
-Route::get('/privacy-policy', PrivacyPolicy::class)->name('privacy-policy');
 Route::get('/terms', Terms::class)->name('terms');
+Route::get('/privacy-policy', PrivacyPolicy::class)->name('privacy-policy');
+Route::get('/refund-policy', RefundPolicy::class)->name('refund-policy');
 Route::view('/checkout', 'checkout')->name('checkout');
