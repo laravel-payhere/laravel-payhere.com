4017b8128511725ce841fc58a6ddeedc73978616
Dasun Tharanga <hello@dasun.dev>
2024-08-11 20:19:18 +0530

    feat: fetch changelog (#2)

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## Version 1.1
+> 11 August, 2024
+
+- Internal improvements and bug fixes.
+
+## Version 1.0
+> 8 August, 2024
+
+- Internal improvements and bug fixes.
+
--- resources/views/livewire/changelog.blade.php
+++ resources/views/livewire/changelog.blade.php
@@ -1,18 +1,18 @@
 <div class="mx-auto mt-10 max-w-5xl px-5">
     <h1 class="my-6 text-start text-2xl text-white sm:text-3xl md:text-4xl">Changelog</h1>
     <div class="text-sm text-white md:text-base">
-        <p>A changelog of the latest Laravel PayHere feature releases, product updates and important bug fixes.</p>
+        <p>A changelog of the latest Laravel PayHere plugin feature releases, and important bug fixes.</p>
 
-        <h2 class="my-6 text-lg sm:text-xl md:text-2xl">v1.0.0-beta.19</h2>
-        <ul class="my-2 list-disc pl-10">
-            <li>Improve the checkout page.</li>
-            <li>Fix the recurring subscription process.</li>
-        </ul>
-
-        <h2 class="my-6 text-lg sm:text-xl md:text-2xl">v1.0.0-beta.18</h2>
-        <ul class="my-2 list-disc pl-10">
-            <li>Improve the checkout page.</li>
-            <li>Fix the recurring subscription process.</li>
-        </ul>
+        @foreach ($releases as $version => $release)
+            <div class="my-6">
+                <span class="text-xs">{{ $release["publishedAt"] }}</span>
+                <h2 class="text-lg sm:text-xl md:text-2xl">{{ __("Version :v", ["v" => $version]) }}</h2>
+                <ul class="my-2 list-disc pl-8 md:pl-10">
+                    @foreach ($release["changes"] as $change)
+                        <li>{{ $change }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endforeach
     </div>
 </div>
