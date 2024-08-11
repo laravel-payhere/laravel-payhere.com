adb309801854df403a2ff2cd99cb0f940073ae1f
Dasun Tharanga <hello@dasun.dev>
2024-08-11 19:49:56 +0530

    wip

--- resources/views/livewire/changelog.blade.php
+++ resources/views/livewire/changelog.blade.php
@@ -1,7 +1,7 @@
 <div class="mx-auto mt-10 max-w-5xl px-5">
     <h1 class="my-6 text-start text-2xl text-white sm:text-3xl md:text-4xl">Changelog</h1>
     <div class="text-sm text-white md:text-base">
-        <p>A changelog of the latest Laravel PayHere plugin features, releases, and important bug fixes.</p>
+        <p>A changelog of the latest Laravel PayHere plugin feature releases, and important bug fixes.</p>
 
         @foreach ($releases as $version => $release)
             <h2 class="my-6 text-lg sm:text-xl md:text-2xl">{{ __("v:version", ["version" => $version]) }}</h2>
