748df9fe43ef3b78eea573598ec4fe2a46a6d84f
Dasun Tharanga <hello@dasun.dev>
2024-08-11 10:48:10 +0530

    wip

--- resources/views/livewire/privacy-policy.blade.php
+++ resources/views/livewire/privacy-policy.blade.php
@@ -1,40 +1,47 @@
 <div class="mx-auto mt-10 max-w-5xl px-5">
     <h1 class="my-6 text-start text-2xl text-white sm:text-3xl md:text-4xl">Privacy Policy</h1>
     <div class="text-sm text-white md:text-base">
-        <p>At Laravel PayHere, we are committed to safeguarding the privacy and security of our customers' personal information. This Privacy Policy outlines how we collect, use, and protect your data when you visit our website or purchase our plugin. By accessing our website or purchasing our plugin, you agree to the practices described in this policy.</p>
+        <p>At Laravel PayHere, we are dedicated to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, and safeguard your data when you visit our website or purchase our plugin. By using our website or purchasing our plugin, you consent to the practices described in this policy.</p>
+
         <h2 class="my-6 text-base sm:text-lg md:text-xl">Information We Collect</h2>
-        <p>When you visit our website, we may collect certain information about you, including:</p>
+        <p>We may collect the following information when you visit our website:</p>
         <ul class="my-2 list-disc pl-10">
-            <li>Personal identification information (such as your name and email address) provided voluntarily by you during the checkout process.</li>
-            <li>Payment and billing information necessary to process your orders, including credit card details, which are securely handled by trusted third-party payment processors.</li>
-            <li>Browsing information, such as your IP address, browser type, and device information, collected automatically using cookies and similar technologies.</li>
+            <li>Personal information such as your name and email address, which you provide during the checkout process.</li>
+            <li>Payment and billing details required to process your order, including credit card information, which is securely managed by trusted third-party payment processors.</li>
+            <li>Browsing data such as your IP address, browser type, and device information, collected automatically through cookies and similar technologies.</li>
         </ul>
-        <h2 class="my-6 text-base sm:text-lg md:text-xl">Use Of Information</h2>
-        <p>We may use the collected information for the following purposes:</p>
+
+        <h2 class="my-6 text-base sm:text-lg md:text-xl">Use of Information</h2>
+        <p>We use the information we collect for the following purposes:</p>
         <ul class="my-2 list-disc pl-10">
-            <li>To process your orders, including issuing licenses.</li>
-            <li>To communicate with you regarding your purchases, provide customer support, and respond to inquiries or requests.</li>
-            <li>To improve our website and plugin based on your feedback.</li>
+            <li>To process your orders and issue licenses.</li>
+            <li>To communicate with you about your purchases, provide customer support, and respond to your inquiries.</li>
+            <li>To enhance our website and plugin based on your feedback.</li>
         </ul>
+
         <h2 class="my-6 text-base sm:text-lg md:text-xl">Information Sharing</h2>
-        <p>We respect your privacy and do not sell, trade, or otherwise transfer your personal information to third parties without your consent, except in the following circumstances:</p>
+        <p>We respect your privacy and will not sell, trade, or otherwise transfer your personal information to third parties without your consent, except in the following cases:</p>
         <ul class="my-2 list-disc pl-10">
             <li>
                 We share your name and email address with
                 <a href="https://www.dasun.dev" target="_blank" rel="noopener noreferrer" class="underline">https://www.dasun.dev</a>
-                to issue the license for using the plugin.
+                to issue the plugin license.
             </li>
-            <li>Legal requirements: We may disclose your information if required to do so by law or in response to valid legal requests or orders.</li>
+            <li>If required by law or in response to legal requests, we may disclose your information.</li>
         </ul>
+
         <h2 class="my-6 text-base sm:text-lg md:text-xl">Data Security</h2>
-        <p>We implement industry-standard security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. However, please be aware that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.</p>
+        <p>We implement industry-standard security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. However, please note that no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.</p>
+
         <h2 class="my-6 text-base sm:text-lg md:text-xl">Cookies and Tracking Technologies</h2>
-        <p>We use cookies and similar technologies to enhance your browsing experience, analyze website traffic, and gather information about your preferences and interactions with our website. You have the option to disable cookies through your browser settings, but this may limit certain features and functionality of our website.</p>
+        <p>We use cookies and similar technologies to improve your browsing experience, analyze website traffic, and gather information about your preferences and interactions with our website. You may disable cookies through your browser settings, but this could limit some features and functionality.</p>
+
         <h2 class="my-6 text-base sm:text-lg md:text-xl">Changes to the Privacy Policy</h2>
-        <p>We reserve the right to update or modify this Privacy Policy at any time. Any changes will be posted on this page with a revised "last updated" date. We encourage you to review this Privacy Policy periodically to stay informed about how we collect, use, and protect your information.</p>
+        <p>We reserve the right to update or modify this Privacy Policy at any time. Any changes will be posted on this page with a revised "last updated" date. We encourage you to review this policy periodically to stay informed about how we collect, use, and protect your information.</p>
+
         <h2 class="my-6 text-base sm:text-lg md:text-xl">Contact Us</h2>
         <p>
-            If you have any questions, concerns, or requests regarding our Privacy Policy or the handling of your personal information, please reach out to Dasun Tharanga at
+            If you have any questions, concerns, or requests regarding our Privacy Policy or the handling of your personal information, please contact Dasun Tharanga at
             <a href="https://www.dasun.dev" target="_blank" rel="noopener noreferrer" class="underline">hello@dasun.dev</a>
             .
         </p>
