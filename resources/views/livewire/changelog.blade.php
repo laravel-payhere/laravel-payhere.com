e3421dc40ea116523474da0372816cfb9a0b3b37
Dasun Tharanga <hello@dasun.dev>
2024-08-11 20:01:59 +0530

    wip

--- resources/views/livewire/changelog.blade.php
+++ resources/views/livewire/changelog.blade.php
@@ -5,9 +5,9 @@
 
         @foreach ($releases as $version => $release)
             <div class="my-6">
-                <span class="rounded-full bg-white bg-opacity-10 px-2 py-1 text-xs">{{ $release["publishedAt"] }}</span>
+                <span class="text-xs">{{ $release["publishedAt"] }}</span>
                 <h2 class="text-lg sm:text-xl md:text-2xl">{{ __("Version :v", ["v" => $version]) }}</h2>
-                <ul class="my-2 list-disc pl-10">
+                <ul class="my-2 list-disc pl-8 md:pl-10">
                     @foreach ($release["changes"] as $change)
                         <li>{{ $change }}</li>
                     @endforeach
