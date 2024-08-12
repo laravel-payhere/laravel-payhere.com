8dd6bdc7e22cced1c7accd0d075afbf3010c2236
Dasun Tharanga <hello@dasun.dev>
2024-08-12 19:18:45 +0530

    ci: update build.sh

--- build.sh
+++ build.sh
@@ -22,5 +22,7 @@ fi
 php composer-setup.php --quiet
 rm composer-setup.php
 
+php composer.phar config http-basic.satis.dasun.dev hello@dasun.dev PAYHERE_LICENSE_KEY
+
 # INSTALL COMPOSER DEPENDENCIES
 php composer.phar install
