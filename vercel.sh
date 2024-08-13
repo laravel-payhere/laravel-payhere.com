b6a874de45dd264f80d832fe773d37d30a3a571f
Dasun Tharanga <hello@dasun.dev>
2024-08-13 19:13:37 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -4,7 +4,7 @@
 yum install -y amazon-linux-extras
 amazon-linux-extras enable php8.1
 yum clean metadata
-yum install php
+yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
 yum install wget
 
 php artisan filament:assets
