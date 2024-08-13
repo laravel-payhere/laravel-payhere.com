3ae1af951444625627dfa51fc81543298541fe93
Dasun Tharanga <hello@dasun.dev>
2024-08-13 19:15:00 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -2,7 +2,7 @@
 
 # Install PHP & WGET
 yum install -y amazon-linux-extras
-amazon-linux-extras enable php8.1
+amazon-linux-extras enable php8.2
 yum clean metadata
 yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
 yum install wget
