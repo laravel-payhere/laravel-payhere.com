bdee5e7fe66b92e861587ddf529f00c2b4896ae3
Dasun Tharanga <hello@dasun.dev>
2024-08-14 23:24:21 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -2,7 +2,7 @@
 yum install -y amazon-linux-extras
 amazon-linux-extras enable php8.2
 yum clean
-yum install php
+yum install php php-{ctype,curl,dom,fileinfo,filter,hash,mbstring,openssl,pcre,pdo,session,tokenizer,xml}
 
 # MIGRATE THE DATABASE
 php artisan migrate
