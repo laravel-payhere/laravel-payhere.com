cba50d9a540a4e4587cc6b8ae4c0be13cc90c7c8
Dasun Tharanga <hello@dasun.dev>
2024-08-18 13:18:31 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -1,18 +1,23 @@
 # INSTALL AMAZON LINUX EXTRAS
+echo "Installing Amazon Linux Extras..."
 yum install -y amazon-linux-extras
 
 # INSTALL PHP
+echo "Enabling PHP 8.2 and installing required PHP packages..."
 amazon-linux-extras enable php8.2
 yum clean metadata
 yum install php php-{ctype,curl,dom,fileinfo,filter,hash,mbstring,openssl,pcre,pdo,session,tokenizer,xml}
 
 # INSTALL POSTGRESQL
+echo "Enabling PostgreSQL 14 and installing PostgreSQL..."
 amazon-linux-extras enable postgresql14
 yum clean metadata
 yum install postgresql postgresql-server
 
 # MIGRATE THE DATABASE
+echo "Running database migrations..."
 php artisan migrate --force
 
 # BUILD ASSETS
+echo "Building assets..."
 npm run build
