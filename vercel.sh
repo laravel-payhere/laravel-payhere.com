52e4075fdf1db8342eea7a2e2bb40ab3d277a45c
Dasun Tharanga <hello@dasun.dev>
2024-08-18 16:51:48 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -2,18 +2,20 @@
 echo "Installing Amazon Linux Extras..."
 yum install -y amazon-linux-extras
 
-# INSTALL PHP & WGET
+# INSTALL PHP
 echo "Enabling PHP 8.2 and installing required PHP packages..."
 amazon-linux-extras enable php8.2
 yum clean metadata
-yum install php php-{ctype,curl,dom,fileinfo,filter,hash,mbstring,openssl,pcre,pdo,session,tokenizer,xml,intl,sodium}
+yum install php php-{ctype,curl,dom,fileinfo,filter,hash,mbstring,openssl,pcre,pdo,session,tokenizer,xml,intl,pgsql}
+
+# INSTALL WGET
 yum install wget
 
-# INSTALL LIBSQL
-curl https://darkterminal.github.io/turso-php-installer/dist/turso-php-installer.phar -o ./turso-php-installer.phar
-chmod +x turso-php-installer.phar
-mv turso-php-installer.phar /usr/local/bin/turso-php-installer
-turso-php-installer install
+# INSTALL POSTGRESQL
+echo "Enabling PostgreSQL 14 and installing PostgreSQL..."
+amazon-linux-extras enable postgresql14
+yum clean metadata
+yum install postgresql-server
 
 # INSTALL COMPOSER
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
@@ -30,11 +32,11 @@ fi
 php composer-setup.php --quiet
 rm composer-setup.php
 
-# INSTALL COMPOSER DEPENDENCIES
-php composer.phar install
-
 # CREATE COMPOSER AUTH.JSON
 php composer.phar config http-basic.satis.dasun.dev hello@dasun.dev $LARAVEL_PAYHERE_LICENSE_KEY
+
+# INSTALL COMPOSER DEPENDENCIES
+php composer.phar install
 
 # MIGRATE THE DATABASE
 echo "Running database migrations..."
