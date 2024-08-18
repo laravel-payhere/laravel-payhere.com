361084a86cef958a67f27f653d12ea8c309161e3
Dasun Tharanga <hello@dasun.dev>
2024-08-18 13:57:16 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -14,6 +14,22 @@ amazon-linux-extras enable postgresql14
 yum clean metadata
 yum install postgresql-server
 
+# INSTALL COMPOSER
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet --install-dir=/usr/local/bin
+
+composer install
+
 # MIGRATE THE DATABASE
 echo "Running database migrations..."
 php artisan migrate --force
