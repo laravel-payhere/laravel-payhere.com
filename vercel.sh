5ec6af5bca268d8e967f615db8d7d99792a15b42
Dasun Tharanga <hello@dasun.dev>
2024-08-18 15:26:14 +0530

    ci: update vercel

--- vercel.sh
+++ vercel.sh
@@ -6,34 +6,7 @@ yum install -y amazon-linux-extras
 echo "Enabling PHP 8.2 and installing required PHP packages..."
 amazon-linux-extras enable php8.2
 yum clean metadata
-yum install php php-{ctype,curl,dom,fileinfo,filter,hash,mbstring,openssl,pcre,pdo,session,tokenizer,xml,intl,pgsql}
-
-# INSTALL WGET
-yum install wget
-
-# INSTALL POSTGRESQL
-echo "Enabling PostgreSQL 14 and installing PostgreSQL..."
-amazon-linux-extras enable postgresql14
-yum clean metadata
-yum install postgresql-server
-
-## INSTALL COMPOSER
-#EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-#ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-#
-#if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-#then
-#    >&2 echo 'ERROR: Invalid installer checksum'
-#    rm composer-setup.php
-#    exit 1
-#fi
-#
-#php composer-setup.php --quiet
-#rm composer-setup.php
-#
-## INSTALL COMPOSER DEPENDENCIES
-#php composer.phar install
+yum install php php-{ctype,curl,dom,fileinfo,filter,hash,mbstring,openssl,pcre,pdo,session,tokenizer,xml,intl}
 
 # MIGRATE THE DATABASE
 echo "Running database migrations..."
