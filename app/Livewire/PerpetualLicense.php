28f70862bc901023bffd26d3eec189bb011bac29
Dasun Tharanga <hello@dasun.dev>
2024-09-22 01:14:41 +0530

    wip

--- app/Livewire/PerpetualLicense.php
+++ app/Livewire/PerpetualLicense.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Perpetual License')]
 final class PerpetualLicense extends Component {}
