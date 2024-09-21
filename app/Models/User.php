8141e70fd7ea0035005b611193144537e2c83210
Dasun Tharanga <hello@dasun.dev>
2024-09-21 23:20:25 +0530

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Dasundev\PayHere\Filament\Contracts\PayHerePanelUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PayHere\Filament\Contracts\PayHerePanelUser;
 
 final class User extends Authenticatable implements PayHerePanelUser
 {
