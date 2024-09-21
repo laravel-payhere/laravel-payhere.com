268d9c51e6b61890053101688e4f744d7cb8519b
Dasun Tharanga <hello@dasun.dev>
2024-09-21 22:40:51 +0530

    wip

--- resources/views/vendor/payhere/checkout.blade.php
+++ resources/views/vendor/payhere/checkout.blade.php
@@ -9,11 +9,6 @@
 
     <title>{{ config("app.name", "Laravel") }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net" />
-    <link href="https://fonts.bunny.net/css?family=ubuntu:400,500,700" rel="stylesheet" />
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600" rel="stylesheet" />
-
     <!-- Scripts -->
     @vite(["resources/css/app.css", "resources/js/app.js"])
 </head>
