698186322566b26b9e0fe2afce53fea6c00ef971
Dasun Tharanga <hello@dasun.dev>
2024-09-22 01:10:08 +0530

    wip

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -64,7 +64,7 @@
         <h2 class="text-center text-2xl font-semibold text-white md:text-3xl xl:text-4xl">Pay once, use forever</h2>
         <p class="mt-2 px-5 text-center text-sm text-white opacity-70 md:px-0 md:text-base xl:text-lg">
             Every purchase includes a
-            <a href="#" class="underline">perpetual fallback license</a>
+            <a href="{{ route('perpetual-license') }}" class="underline">perpetual fallback license</a>
             that you can use forever.
         </p>
         <div class="mx-auto grid max-w-5xl grid-cols-1 lg:grid-cols-3">
