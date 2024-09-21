d6e230d6267480104d418bcbe58fbdf466c1ef4e
Dasun Tharanga <hello@dasun.dev>
2024-09-21 22:58:49 +0530

    wip

--- app/Http/Controllers/Checkout.php
+++ app/Http/Controllers/Checkout.php
@@ -10,6 +10,7 @@ class Checkout extends Controller
     {
         return PayHere::builder()
             ->guest()
+            ->title('Perpetual License (1 Year)')
             ->amount(30000)
             ->checkout();
     }
