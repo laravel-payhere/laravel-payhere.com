01b0776fd13260b1ee31f9e63be833d82119dad5
Dasun Tharanga <hello@dasun.dev>
2024-09-22 16:56:19 +0530

    wip

--- app/Listeners/PurchaseConfirmed.php
+++ app/Listeners/PurchaseConfirmed.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 use PayHere\Events\PaymentVerified;
 use PayHere\Models\Payment;
@@ -24,6 +25,17 @@ class PurchaseConfirmed
     private function createLicense(Payment $payment): void
     {
         $customer = $this->fetchCustomer($payment);
+
+        $token = config('license-api.token');
+
+        Http::withToken($token)->post('https://www.dasun.dev/licese/api', [
+            'name' => $customer['first_name'] . ' ' . $customer['last_name'],
+            'email' => $customer['email'],
+            'license' => 'Perpetual License (1 Year)',
+            'purchasable_type' => 'App\Models\Package',
+            'purchasable_id' => 1,
+            'expires_at' => Carbon::now()->addYear()
+        ]);
     }
 
     /**
