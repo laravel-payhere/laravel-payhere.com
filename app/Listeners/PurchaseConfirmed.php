85d346e14940077e8e05e003e24734f824d60951
Dasun Tharanga <hello@dasun.dev>
2024-09-22 16:56:45 +0530

    wip

--- app/Listeners/PurchaseConfirmed.php
+++ app/Listeners/PurchaseConfirmed.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Listeners;
 
 use Illuminate\Http\Client\ConnectionException;
@@ -8,7 +10,7 @@ use Illuminate\Support\Facades\Http;
 use PayHere\Events\PaymentVerified;
 use PayHere\Models\Payment;
 
-class PurchaseConfirmed
+final class PurchaseConfirmed
 {
     public function handle(PaymentVerified $event): void
     {
@@ -20,8 +22,6 @@ class PurchaseConfirmed
     /**
      * Create a perpetual license for 1 year.
      *
-     * @param Payment $payment
-     * @return void
      * @throws ConnectionException
      */
     private function createLicense(Payment $payment): void
@@ -31,20 +31,17 @@ class PurchaseConfirmed
         $token = config('license-api.token');
 
         Http::withToken($token)->post('https://www.dasun.dev/licese/api', [
-            'name' => $customer['first_name'] . ' ' . $customer['last_name'],
+            'name' => $customer['first_name'].' '.$customer['last_name'],
             'email' => $customer['email'],
             'license' => 'Perpetual License (1 Year)',
             'purchasable_type' => 'App\Models\Package',
             'purchasable_id' => 1,
-            'expires_at' => Carbon::now()->addYear()
+            'expires_at' => Carbon::now()->addYear(),
         ]);
     }
 
     /**
      * Fetch the customer details from PayHere using the provided payment.
-     *
-     * @param Payment $payment
-     * @return array|null
      */
     private function fetchCustomer(Payment $payment): ?array
     {
