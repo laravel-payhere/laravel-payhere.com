5e55001b719f9ff989265248d138c5fa61d8b2ea
Dasun Tharanga <hello@dasun.dev>
2024-09-22 18:36:24 +0530

    ci: update envoy tasks

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -14,28 +14,28 @@ maintenance-mode-off
 @endstory
 
 @task('update-code')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 git pull origin main
 @endtask
 
 @task('install-dev-dependencies')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 composer install
 npm install
 @endtask
 
 @task('install-prod-dependencies')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 composer install --optimize-autoloader --no-dev
 @endtask
 
 @task('maintenance-mode-on')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 php artisan down --secret=c2fBxu9p8o
 @endtask
 
 @task('optimize')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
@@ -43,26 +43,26 @@ php artisan icons:cache
 @endtask
 
 @task('build-assets')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 npm run build
 @endtask
 
 @task('migrate-database')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 php artisan migrate --force
 @endtask
 
 @task('restart-queue-workers')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 php artisan queue:restart
 @endtask
 
 @task('maintenance-mode-off')
-cd /home/laravel/laravel-payhere
+cd /home/laravel/laravel-payhere.com
 php artisan up
 @endtask
 
 @task('build-docs')
-cd /home/laravel/laravel-payhere/docs
+cd /home/laravel/laravel-payhere.com/docs
 npm run docs:build
 @endtask
