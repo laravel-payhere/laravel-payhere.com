9d4f00620cccdbbde2311b2048045264e9743892
Dasun Tharanga <hello@dasun.dev>
2024-09-24 16:20:16 +0530

    fix: manifest filename

--- resources/views/vendor/payhere/return.blade.php
+++ resources/views/vendor/payhere/return.blade.php
@@ -9,7 +9,7 @@
             <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("favicon-32x32.png") }}" />
             <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon-16x16.png") }}" />
             <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("apple-touch-icon.png") }}" />
-            <link rel="manifest" href="{{ asset("site.webmanifest") }}" />
+            <link rel="manifest" href="{{ asset("webmanifest.json") }}" />
 
             <!-- Fonts -->
             <link rel="dns-prefetch" href="//fonts.bunny.net" />
