bd89bb19800c5f8dd12ca9fa8f3988eab24f12c5
Dasun Tharanga <hello@dasun.dev>
2024-10-04 19:06:10 +0530

    feat: new license sold email

---
+++ resources/views/emails/new-license-purchased.blade.php
@@ -0,0 +1,5 @@
+@component("mail::message")
+    Hey Dasun! 👋 Just wanted to share some cool news—someone grabbed a license for the Laravel PayHere plugin! 👀 Cheers,
+    <br />
+    {{ config("app.name") }}
+@endcomponent
