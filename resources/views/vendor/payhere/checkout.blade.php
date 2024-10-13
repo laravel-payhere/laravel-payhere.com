499feb9ed405b9e42c28a890a7a84dc9e7e791e3
Dasun Tharanga <hello@dasun.dev>
2024-10-13 21:36:14 +0530

    feat: add umami script

--- resources/views/vendor/payhere/checkout.blade.php
+++ resources/views/vendor/payhere/checkout.blade.php
@@ -238,5 +238,6 @@
             </div>
         </main>
         <x-support-bubble />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="c84ecd58-6627-4d89-adb4-dbccb72dfb50"></script>
     </body>
 </html>
