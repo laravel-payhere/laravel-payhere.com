948320d32b1f7823b58022c274c863293984e764
Dasun Tharanga <hello@dasun.dev>
2024-11-01 22:47:48 +0530

    feat: add `umamiJS` blade directive (#16)

--- resources/views/vendor/payhere/return.blade.php
+++ resources/views/vendor/payhere/return.blade.php
@@ -35,6 +35,6 @@
                 </div>
             </div>
         </body>
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="c84ecd58-6627-4d89-adb4-dbccb72dfb50"></script>
+        @umamiJS
     </html>
 </html>
